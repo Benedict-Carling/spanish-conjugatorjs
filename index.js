76e3b5f7e1eb83f060bf4b75d6e16d9875e0e84f
Benedict Carling <benedict.carling18@imperial.ac.uk>
2020-09-19 20:18:55 +0100

    implements irregular dictionary

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+const irregular_dictionary = require('./irregulars/irregulars_dictionary')
+const subjunctive_present = require('./tenses/subjunctive/present');
+
 function SpanishConjugator(rootverb,tense,mood,pronoun) {
-    return 'Spanish Conjugator is live';
+    // Make all inputs lower case
+    //console.log(irregular_dictionary.irregulars_dictionary.rootverb)
+    // Queries Irregular database
+    try {
+        //let my_irregulars = irregular_dictionary.irregulars_dictionary;
+        let conjugation = irregular_dictionary.irregulars_dictionary[rootverb][mood][tense][pronoun];
+        return conjugation;
+    } catch (error) {
+        //Add all tenses
+        return subjunctive_present.subjuctive_present("hablar","yo");
+    }
 }
+
+console.log(SpanishConjugator("hablar","present","subjunctive","yo"));
+console.log(SpanishConjugator("ir","present","indicitive","yo"));
 
 module.exports.SpanishConjugator = SpanishConjugator;
